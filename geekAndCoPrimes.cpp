3d5f4780ff21798f59eda600b93162534c5176f7
shivom007 <shivmahto37@gmail.com>
2022-09-24 04:20:46 +0530

    Geek and Co-prime Number

---
+++ geekAndCoPrimes.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    long long sum(long long N, long long M){
+        // code here
+        //_gcd(N,M) will give the Greatest common Divisor in Log(min(N,M)) time and N and M are the input 
+        return (M+N)/__gcd(N,M);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        long long N, M;
+        cin >> N >> M;
+        Solution obj;
+        cout << obj.sum(N, M) << "\n";
+    }
+}
+  // } Driver Code Ends
